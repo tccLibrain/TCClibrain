621944134dd24df5bbf1a288faceb6c310e88328
tccLibrain <libraintcc@gmail.com>
2025-10-28 02:03:58 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
 // ================================
 // TESTE CRÍTICO DE ROTAS
 // ================================
